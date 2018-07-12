47c203d4a3221f4a96a52c8cc8aae95b164a6bc6
KevGri98 <kev.gri32@gmail.com>
2018-07-12 12:49:10 -0700

    Added FireBase

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+//Initialize FireBase
+var config = {
+  apiKey: "AIzaSyBYXaPL71qCVdjY97xGOCfU-z4NGf1u49E",
+  authDomain: "idunnoknow-af8c7.firebaseapp.com",
+  databaseURL: "https://idunnoknow-af8c7.firebaseio.com",
+  projectId: "idunnoknow-af8c7",
+  storageBucket: "idunnoknow-af8c7.appspot.com",
+  messagingSenderId: "289868028838"
+};
+firebase.initializeApp(config);
